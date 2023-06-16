f7400887e50216d6b296ae2b4cc738943ab0be57
bye-on <codnjs842@naver.com>
2023-06-16 20:53:31 +0900

    Make Class #3

---
+++ src/Management.java
@@ -0,0 +1,21 @@
+public class Management {
+    private String userID;
+    private String userScheduleInfo;
+    private String userResumeInfo;
+    private int userApproach;
+
+    public Management() {
+        this.userID = "";
+        this.userScheduleInfo = "";
+        this.userResumeInfo = "";
+        this.userApproach = 0;
+    }
+
+    public void deletion(String userID) {
+
+    }
+
+    public void showUserList() {
+
+    }
+}
